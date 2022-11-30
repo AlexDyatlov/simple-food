891858ed018e71a052a4729cf5326bd93637a9ea
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-11-30 19:06:25 +0300

    Added react-redux, redux-toolkit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { createStore } from './store/createStore';
 
 import App from './App';
 
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore();
 
-root.render(
-  <BrowserRouter>
-    {/* <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
-  </BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
