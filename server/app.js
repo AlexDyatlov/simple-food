aeb5b33630ef9272e90edb3164400acf683cd645
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-19 10:54:40 +0300

    Setting up the server and work environment

--- server/app.js
+++ server/app.js
@@ -1,1 +1,12 @@
-console.log('Server');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const chalk = require('chalk');
+
+const app = express();
+
+const PORT = config.get('port') ?? 8080;
+
+app.listen(PORT, () =>
+  console.log(chalk.green(`Server has been started on port ${PORT}...`))
+);
