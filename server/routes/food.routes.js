334134fffe85265630d2a0d6765f667b43320ded
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-20 11:09:12 +0300

    Getting categories and food with a get request

--- server/routes/food.routes.js
+++ server/routes/food.routes.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-router.get('/', async (req, res) => {});
+const Food = require('../models/Food');
+
+router.get('/', async (req, res) => {
+  try {
+    const list = await Food.find();
+    res.status(200).send(list);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.'
+    });
+  }
+});
 
 router.delete('/:productId', async (req, res) => {});
 
