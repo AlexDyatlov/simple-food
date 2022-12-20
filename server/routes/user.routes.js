ab382153a8997b22c4050060452b31c9d8ce9caf
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-20 16:05:24 +0300

    Implemented get method for users and creating Middleware for Authorization

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-router.get('/', async (req, res) => {
+const User = require('../models/User');
+const auth = require('../middleware/auth.middleware');
 
+router.get('/', auth, async (req, res) => {
+  try {
+    const list = await User.find();
+    res.status(200).send(list);
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.'
+    });
+  }
 });
 
 module.exports = router;
