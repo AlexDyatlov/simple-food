4f29ea4c94a9ef525b3a1357c3a8926700c92265
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-21 15:50:30 +0300

    Creating logic for food, implemented editing and deleting food

--- server/routes/food.routes.js
+++ server/routes/food.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const Food = require('../models/Food');
+const User = require('../models/User');
+const auth = require('../middleware/auth.middleware');
 
 router.get('/', async (req, res) => {
   try {
@@ -14,8 +16,46 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.delete('/:productId', async (req, res) => {});
+router.delete('/:productId', auth, async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const removedFood = await Food.findById(productId);
 
-router.patch('/:productId', async (req, res) => {});
+    const adminUser = await User.find({ isAdmin: true });
+    const adminUserId = adminUser[0]._id.toString();
+
+    if (adminUserId === req.user._id) {
+      await removedFood.remove();
+      return res.send(null);
+    } else {
+      return res.status(403).json({ message: 'Доступ запрещен.' });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.'
+    });
+  }
+});
+
+router.patch('/:productId', auth, async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const adminUser = await User.find({ isAdmin: true });
+    const adminUserId = adminUser[0]._id.toString();
+
+    if (adminUserId === req.user._id) {
+      const updatedFood = await Food.findByIdAndUpdate(productId, req.body, {
+        new: true
+      });
+      res.send(updatedFood);
+    } else {
+      return res.status(403).json({ message: 'Доступ запрещен.' });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.'
+    });
+  }
+});
 
 module.exports = router;
