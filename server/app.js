556d257db7010ebdf303bd21dad7872f605a4acb
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-25 15:38:06 +0300

    Implemented Production mode for the server

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const config = require('config');
 const chalk = require('chalk');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 const initDatabase = require('./startUp/initDatabase');
 const routes = require('./routes');
@@ -18,6 +19,16 @@ app.use('/api', routes);
 
 const PORT = config.get('port') ?? 8080;
 const urlMongo = process.env.MONGO_URI;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client')));
+
+  const indexPath = path.join(__dirname, 'client', 'index.html');
+
+  app.get('*', (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
